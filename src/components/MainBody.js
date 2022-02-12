7b39859b6be4f4ed3ff37534dbb84b655e3ca4a4
Alexander Roldan <alexroldan49@gmail.com>
2022-02-11 21:30:48 -0500

    made the coma function dynamic with other numbers. adjusted css.

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -3,34 +3,49 @@ import react from "react";
 
 function MainBody({totalCases, allStats}){
 
-    let mill = ""
-    let cases = totalCases + ""
-        function comas(totalCases){
-            for(let i=0; i < (totalCases + "").length; i++){
-                if( (i  % 3 === 0 && i > 0)){
-                    mill += ","
-                    mill += cases[i]
+    // let mill = ""
+    
+        function comas(num){
+            let newNum = ""
+            let oldNum = num + ""
+            console.log(oldNum)
+            let byThree = 0
+            for(let i= oldNum.length -1; i >= 0 ; i--){
+                console.log(oldNum[i])
+                console.log(newNum)
+                if( byThree  % 3 === 0 && byThree > 0){
+                    newNum += ","
+                    newNum += oldNum[i]
+                    byThree++
+                    console.log(newNum)
                 }else{
-                    mill += cases[i]
+                    newNum += oldNum[i]
+                    byThree ++
                 }
             }
+            return newNum.split("").reverse().join("")
+            
         }
     
-        comas()
+     let cases = comas(totalCases)
+     let todaysCases = comas(allStats.todayCases)
+     console.log(allStats.todayCases)
 
     return(
         <>
             <h3 className="bold" >TOTAL COVID-19 CASES</h3>
-            <h1 className="total-count" >{mill}</h1>
+            <h1 className="total-count" >{cases}</h1>
+            <h3 style={{fontWeight: "800", margin: "0", padding: "0"}} >Throughout the world</h3>
             <div className="main-grid" >
                 <div className="main-stat" >
-                    <h2>Today's Cases</h2> 
+                    <h2>Today's Cases</h2>
+                    <h3>{`Today there have been ${todaysCases} additional cases throughout the world`}</h3>
                 </div>
                 <div className="main-stat" >
                     <h2>Today's Recoveries</h2> 
                 </div>
                 <div className="main-stat" >
-                    <h2>Total Tests</h2> 
+                    <h2 className="">Total Tests</h2> 
                 </div>
             </div>
         </>  
